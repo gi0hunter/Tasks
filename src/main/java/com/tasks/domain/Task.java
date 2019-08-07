ec518f993db7ff34a3fd06f7c63897d5962379c9
Giovani Correa <giovanicorrea@MacBook-Air-de-Giovani.local>
2019-08-07 08:17:02 -0300

    Atualizando application propriets

--- src/main/java/com/tasks/domain/Task.java
+++ src/main/java/com/tasks/domain/Task.java
@@ -3,10 +3,10 @@ package com.tasks.domain;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
