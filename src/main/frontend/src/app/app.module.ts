53189adfc83eadd46294bb67ff7c1d91f4d192bf
Giovani Correa <giovanicorrea@MacBook-Air-de-Giovani.local>
2019-08-08 13:17:21 -0300

    SUbindo Angular

---
+++ src/main/frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    TasksAddComponent,
+    TasksListComponent
+  ],
+  imports: [
+      BrowserModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot()
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
