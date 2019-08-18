b254253cd2bb3cd2f42c3c3e2c4ae407e30291b9
Giovani Correa <giovanicorrea@MacBook-Air-de-Giovani.local>
2019-08-18 08:53:56 -0300

    Update

--- src/main/java/com/tasks/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tasks.repository;
-
-import com.tasks.domain.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
--- src/main/java/com/tasks/service/TaskService.java
+++
@@ -1,9 +0,0 @@
-package com.tasks.service;
-
-import com.tasks.domain.Task;
-
-public interface TaskService {
-
-    Iterable<Task> list();
-    Task save(Task task);
-}
